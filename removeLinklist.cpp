d9c9c1857372ada4ec8878311241bbf52c9517de
Md. Refayatul Islam <refayatul@outlook.com>
2023-08-05 20:33:24 +0600

    add option for All 3 delete

--- removeLinklist.cpp
+++ removeLinklist.cpp
@@ -1,68 +1,83 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define ll long long
-#define endl '\n'
 
-
-class Node{
+class Node {
 public:
     int data;
-    Node *next;
-    Node(int value){
+    Node* next;
+    Node(int value) {
         this->data = value;
         next = NULL;
     }
-
 };
 
-void printLiskList(Node *head){
-    while(head != NULL)
-    {
-        cout<<head->data<< " ";
+void printLinkedList(Node* head) {
+    while (head != NULL) {
+        cout << head->data << " ";
         head = head->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void removeLast(Node *&head){
-    Node *current = head;
-    while(current->next->next != NULL){
-        current = current->next; 
+void removeLast(Node* head) {
+    if (head == NULL || head->next == NULL)
+        return;
+
+    Node* current = head;
+    while (current->next->next != NULL) {
+        current = current->next;
     }
+    delete current->next;
     current->next = NULL;
-    cout<<"After removing Last  :";
+    cout << "After removing Last: ";
+    printLinkedList(head);
 }
 
-void removeFirst(Node *&head){
+void removeFirst(Node*& head) {
+    if (head == NULL)
+        return;
+
+    Node* temp = head;
     head = head->next;
-    cout<<"After removing First :";
+    delete temp;
+    cout << "After removing First: ";
+    printLinkedList(head);
 }
 
 
+void removeGivenPosition(Node *head,int pos){
+    Node *current = head;
+    if(pos == 1){
+        removeFirst(head);
+        return;
+    }
+    for(int i = 0;i < pos - 2;i++){   
+        head = head ->next;
+    }
+     if (head == NULL || head->next == NULL) {
+         cout << "Invalid position!" << endl;
+         return;
+     }
+      head->next = head->next->next;
 
+      cout<<"After removing value from "<< pos <<" position : ";
+}
 
-
-
-
-
-
-int main()
-{   
-    
-    Node *head = new Node(3);
+int main() {
+    Node* head = new Node(3);
     head->next = new Node(4);
     head->next->next = new Node(5);
-    head->next->next->next= new Node(6);
-    printLiskList(head);
-    removeLast(head);
-    printLiskList(head);
+    head->next->next->next = new Node(6);
+    head->next->next->next->next = new Node(7);
+
+    cout << "Original Linked List: ";
+    printLinkedList(head);
+
+    
+    removeGivenPosition(head, 3);
+    printLinkedList(head);
     removeFirst(head);
-    printLiskList(head);
-
-   
-
-
-
+    removeLast(head);
 
     return 0;
 }
